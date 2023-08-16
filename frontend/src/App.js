847c9f62c9ae5fab401b285325f44619b43fd285
Atilio <atilio.jesus@grupoboticario.com.br>
2023-08-16 07:47:53 -0300

    Update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,32 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function Counter(){
-  const [counterNumber, setCounterNumber] = useState(0);
+    const [counterNumber, setCounterNumber] = useState(0);
 
-  const plusplus = ()=>{
-    setCounterNumber(counterNumber+1);
-  }
-  const minusminus = ()=>{
-    if(counterNumber>0){
-      setCounterNumber(counterNumber-1);
+    const plusplus = ()=>{
+        setCounterNumber(counterNumber+1);
     }
-  }
+    const minusminus = ()=>{
+        if(counterNumber>0){
+            setCounterNumber(counterNumber-1);
+        }
+    }
 
-  const reset = ()=>{
-    setCounterNumber(0);
-  }
-
-  return (
-    <div style={{ display:'flex', alignItems:'center', flexDirection: 'column' }}>
-      <h1>Counter</h1>
-      <h3>The Number is: {counterNumber}</h3>
-      <div>
-        <button onClick={plusplus}>+</button>
-        <button onClick={minusminus}>-</button>
-        <button onClick={reset}>Reset</button>
-      </div>
-    </div>
-  );
+    const reset = ()=>{
+        setCounterNumber(0);
+    }
+    return (
+        <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+            <h1>Counter </h1>
+            <h3>Number is: {counterNumber}</h3>
+            <div>
+                <button onClick={plusplus}>+</button>
+                <button onClick={minusminus}>-</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </div>
+    );
 }
 
 export default Counter;
